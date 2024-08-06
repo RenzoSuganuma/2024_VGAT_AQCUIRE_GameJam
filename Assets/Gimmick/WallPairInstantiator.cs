5ac39f0c20c8eae9b947da2798cbaf59bc05b3dd
takasimamasahiro <134473651+takasimamasahiro@users.noreply.github.com>
2024-08-06 23:55:39 +0900

    [add] [Updeet] [上下の壁のランダム生成をつくった]

--- Assets/Gimmick/WallPairInstantiator.cs
+++ Assets/Gimmick/WallPairInstantiator.cs
@@ -5,25 +5,20 @@ using UnityEngine;
 public class WallPairInstantiator : MonoBehaviour
 {
     GameSystem _GameSystem;
-    [SerializeField, Header("壁をランダムで選択して生成する")] GameObject[] _WallSpon;
-    [SerializeField, Header("壁の生成場所")] Vector3 _SponPosition;
-    int _Random;
+    [SerializeField, Header("壁をランダムで選択して生成する上壁")] GameObject[] _UpperWall;
+    [SerializeField, Header("壁をランダムで選択して生成する下壁")] GameObject[] _UnderWall;
     // Start is called before the first frame update
     void Start()
     {
         _GameSystem = GameObject.FindAnyObjectByType<GameSystem>();
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
         if (_GameSystem is not null && !_GameSystem.IsPausing)
         {
-            GameObject childObject = Instantiate(_WallSpon[Random.Range(0, _WallSpon.Length)], this.transform);
-            childObject.transform.localPosition = new Vector3(0, 2, 0);
-            childObject.transform.localRotation = Quaternion.identity;
-            childObject.transform.localScale = new Vector3(1, 1, 1);
+            int UpperWallRandom = Random.Range(0, _UpperWall.Length - 1);
+            Debug.Log($"{UpperWallRandom } 上");
+            int UnderWallRandom = Random.Range(0, _UnderWall.Length - 1);
+            Debug.Log($"{UnderWallRandom}　下");
+            _UpperWall[UpperWallRandom].SetActive(true);
+            _UnderWall[UnderWallRandom].SetActive(true);
         }
-
     }
 }
