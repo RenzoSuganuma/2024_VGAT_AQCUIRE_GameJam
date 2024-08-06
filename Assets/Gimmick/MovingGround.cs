4779cff149d312adf20e1f6740e44c597da2da92
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2024-08-06 17:51:04 +0900

    [ update ]

---
+++ Assets/Gimmick/MovingGround.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingGround : MonoBehaviour
+{
+    float _velocity = 0f;
+
+    [SerializeField, Header("壁の動く方向 *動かしたい方向に１を入れてください")]
+    Vector3 _WallMoveVector = Vector3.zero;
+
+    [SerializeField, Header("壁の動く速度")] float _Speed = 5f;
+    GameSystem _gameSystem;
+    private Vector3 _destructionPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ここから壁のスピードを参照する[
+        _gameSystem = GameObject.FindAnyObjectByType<GameSystem>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!_gameSystem.IsPausing)
+        {
+            _velocity = _Speed;
+
+            this.transform.position += new Vector3(_WallMoveVector.x * _Speed * _velocity * Time.deltaTime,
+                _WallMoveVector.y * _Speed * _velocity * Time.deltaTime,
+                _WallMoveVector.z * _Speed * _velocity * Time.deltaTime);
+        }
+
+        if (transform.position.x < _destructionPos.x)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void SetDestructionPos(Vector3 pos)
+    {
+        _destructionPos = pos;
+    }
+}
--- Assets/Gimmick/WallPairGenerator.cs
+++ Assets/Gimmick/WallPairGenerator.cs
@@ -1,15 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class WallPairGenerator : MonoBehaviour
 {
-    [SerializeField, Header("壁のPrefabを入れてください")] GameObject _WallPrefab;
+    [SerializeField, Header("壁のPrefabを入れてください")]
+    GameObject _WallPrefab;
+
     GameSystem _GameSystem;
     public int _Time;
-    [SerializeField, Header("範囲を決めてください 2まで")] Vector3[] _Pos1;
-    [SerializeField, Header("壁の消える距離")] float _Dellte = 20f;
 
+    [SerializeField, Header("範囲を決めてください 2まで")]
+    Vector3[] _Pos1;
+
+    [SerializeField] private Vector3 _destructionBorder;
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position + _destructionBorder, 1);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -18,28 +31,29 @@ public class WallPairGenerator : MonoBehaviour
         StartCoroutine(Wall());
     }
 
+    private void Update()
+    {
+    }
+
     // Update is called once per frame
     private IEnumerator Wall()
     {
         while (true)
         {
             yield return new WaitForSeconds(_Time);
-            float x = Random.Range(_Pos1[0].x, _Pos1[1].x);
+            float x = transform.position.x;
             float y = Random.Range(_Pos1[0].y, _Pos1[1].y);
             Vector3 pos = new Vector3(x, y, 0);
             if (!_GameSystem.IsPausing)
             {
-                Instantiate(_WallPrefab, pos, Quaternion.identity);
+                var obj = Instantiate(_WallPrefab, pos, Quaternion.identity);
+                var c = obj.GetComponent<MovingWall>();
+                c.SetDestructionPos(transform.position + _destructionBorder);
             }
-            //Debug.Log($"{pos.ToString()}");
         }
     }
+
     private void FixedUpdate()
     {
-        if (this.transform.position.x <= _Dellte)
-        {
-            Destroy(this.gameObject);
-        }
     }
 }
-
