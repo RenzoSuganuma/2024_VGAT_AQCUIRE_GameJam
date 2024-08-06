2cdf2a7577d5a7582582fb9eae5d1a54abe00f97
KakinumaYuzuki <V.proYuzu23@gmail.com>
2024-08-06 17:01:04 +0900

    [ fix, add ] Playerの跳ぶ挙動を修正、Playerをプレハブ化

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -1,31 +1,74 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
-public class PlayerController : MonoBehaviour
+public sealed class PlayerController : MonoBehaviour
 {
-    // GameSystem/isPausing
-    [SerializeField] private float _flyPower = 5f;
-    [SerializeField] private float _jumpPower = 0.5f;
-    [SerializeField] private float _maxFallSpeed = 15f;
-    [SerializeField] private PlayerMoveState _currentMoveState = PlayerMoveState.Fly;
+    [SerializeField, Header("飛ぶときに加える力")] private float _flyPower = 5f;
+    [SerializeField, Header("跳ぶときに加える力")] private float _jumpPower = 0.5f;
+    [SerializeField, Header("現在の操作状態")] private PlayerMoveState _currentMoveState = PlayerMoveState.Fly;
+    [SerializeField, Header("切り替わる時間の最小時間(秒)")] private float _minChangeTime = 3f;
+    [SerializeField, Header("切り替わる時間の最大時間(秒)")] private float _maxChangeTime = 10f;
+    [SerializeField, Header("跳ぶときに力を加え続ける高さの限界")] private float _y = 0.5f;      
     
+    // ギミックから
+    [SerializeField, Header("飛ぶときにアウトな壁のタグ")] private string _flyGOTag = "";
+    [SerializeField, Header("跳ぶときにアウトな壁のタグ")] private string _jumpGOTag = "";
+    
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _flyAudioClip;
+    [SerializeField] private AudioClip _jumpAudioClip;
+    [SerializeField] private AudioClip _clashAudioClip;
+
     private Rigidbody _rb;
+    private GameSystem _gameSystem;
     private bool _isGrounded = false;
     private bool _canJump = false;
+    private bool _isInputJump = false;
+    private bool _isKeyDown = false;
+    private bool _isKeyUp = false;
     
-    void Start()
+    private void Start()
     {
         _rb = GetComponent<Rigidbody>();
-        Debug.Log(_currentMoveState);
+        //Debug.Log(_currentMoveState);
+        _gameSystem = GameObject.FindObjectOfType<GameSystem>().GetComponent<GameSystem>();
+        _audioSource = GetComponent<AudioSource>();
+        // ランダムに飛ぶと跳ぶを変更
+        //StartCoroutine("ChangePlayerMoveState");
+        if (_flyGOTag is null || _jumpGOTag is null)
+        {
+            Debug.LogWarning("ゲームオーバー用のタグがnullです");
+        }
     }
 
-    void Update()
+    private void FixedUpdate()
     {
-        Debug.Log(_isGrounded);
+        //Debug.Log($"接地：{_isGrounded}");
+        //Debug.Log(_currentMoveState);
+        //Debug.Log(_gameSystem.IsPausing);
+        if (_gameSystem is null)
+        {
+            Debug.Log("ゲームシステムがnullです");
+        }
+        
+        PlayerMove();
+        // ポーズしていないときは動ける
+        if (_gameSystem is not null && !_gameSystem.IsPausing)
+        {
+            PlayerMove();
+        }
+    }
 
+    private void Update()
+    {
+        _isKeyDown = Input.GetKeyDown(KeyCode.Space);
+        _isKeyUp = Input.GetKeyUp(KeyCode.Space);
+        InputJump();
+    }
+
+    private void PlayerMove()
+    {
         switch (_currentMoveState)
         {
             case PlayerMoveState.Fly:
@@ -43,10 +86,11 @@ public class PlayerController : MonoBehaviour
     private void FlyPlayerMove()
     {
         
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (_isKeyDown)
         {
             //Debug.Log("F");
-            _rb.velocity = new Vector3(0, _flyPower, 0);
+            _rb.velocity = new Vector3(0, _flyPower, 0); // ToDo:
+            _audioSource.PlayOneShot(_flyAudioClip);
         }
     }
 
@@ -55,40 +99,96 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     private void JumpPlayerMove()
     {
-        if (transform.position.y < 1f)
+        //InputJump();
+        if (_isInputJump && transform.position.y < _y)
         {
-            if (Input.GetKey(KeyCode.Space))
-            {
-                //Debug.Log("J");
-                _canJump = true;
-            }            
+            _canJump = true;
+        }
+        else
+        {
+            _canJump = false;
+        }
+        
+        if (_isGrounded && _isKeyDown)
+        {
+            _audioSource.PlayOneShot(_jumpAudioClip);
         }
         
         if (_canJump)
         {
-            if (transform.position.y < 1f)
-            {
-                _rb.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
-                _isGrounded = false;
-                _canJump = false;
-            }
-            else
-            {
-                _canJump = false;
-            }
+            _rb.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse); // ToDo
+            _isGrounded = false;
+            _canJump = false;
+        }
+    }
+    
+    /// <summary>
+    /// 長押しと短押しのフラグ
+    /// </summary>
+    private void InputJump()
+    {
+        if (_isKeyDown && _isGrounded)
+        {
+            _isInputJump = true;
+        }
+        else if (_isKeyUp || transform.position.y > _y)
+        {
+            _isInputJump = false;
         }
     }
 
+    /// <summary>
+    /// ゲームオーバー時の処理
+    /// </summary>
+    private void GameOver()
+    {
+        _gameSystem.NotifyPlayerIsDeath();
+        _audioSource.PlayOneShot(_clashAudioClip);
+    }
+
+    /// <summary>
+    /// 接地判定とゲームオーバーの接触判定
+    /// </summary>
+    /// <param name="other"></param>
     private void OnCollisionEnter(Collision other)
     {
-        if (_currentMoveState == PlayerMoveState.Jump)
+        string tag = other.gameObject.tag;
+        if (_currentMoveState == PlayerMoveState.Fly)
         {
-            if (other.gameObject.tag == "Ground")
+            // Flyの時にぶつかったらぶつかったらダメなオブジェクトにぶつかるとゲームオーバー
+            if (_flyGOTag is not null && tag == _flyGOTag)
+            {
+                GameOver();
+            }
+        }
+        else if (_currentMoveState == PlayerMoveState.Jump)
+        {
+            // Jumpの時にぶつかったらぶつかったらダメなオブジェクトにぶつかるとゲームオーバー
+            if (_jumpGOTag is not null && tag == _jumpGOTag)
+            {
+                GameOver();
+            }
+            if (tag == "Ground")
             {
                 _isGrounded = true;
             }            
         }
     }
     
-    // Notify
+    /// <summary>
+    /// ランダムな時間経過で操作方法を切り替える
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator ChangePlayerMoveState()
+    {
+        while (true)
+        {
+            _currentMoveState = PlayerMoveState.Fly;
+            Debug.Log("Flyに変更");
+            yield return new WaitForSeconds(Random.Range(_minChangeTime, _maxChangeTime));
+            Debug.Log("Jumpに変更");
+            _currentMoveState = PlayerMoveState.Jump;
+            yield return new WaitForSeconds(Random.Range(_minChangeTime, _maxChangeTime));
+        }
+    }
 }
