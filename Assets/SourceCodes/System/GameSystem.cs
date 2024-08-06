941923d6ea99ede20283777ab83ef86792a5a964
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2024-08-06 10:48:31 +0900

    [ add ] scene の追加とシステム系のスクリプトの追加

--- Assets/SourceCodes/System/GameSystem.cs
+++ Assets/SourceCodes/System/GameSystem.cs
@@ -1,8 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gamesystem : MonoBehaviour
+/// <summary>
+///  ゲームシステムクラス
+/// </summary>
+public class GameSystem : MonoBehaviour
 {
-    
+    /// <summary>
+    /// ポーズ入力が入ったときに呼び出してほしいメソッドはここ
+    /// </summary>
+    public event Action ToDoOnPause;
+    /// <summary>
+    /// リジューム入力が入ったときに呼び出してほしいメソッドはここ
+    /// </summary>
+    public event Action ToDoOnResume;
+
+    /// <summary>
+    /// ジャンプ入力 【トリガー】
+    /// </summary>
+    public bool IsJumping => Input.GetKeyDown(KeyCode.Space);
+
+    /// <summary>
+    /// プレイヤのスピードを返す
+    /// </summary>
+    public float PlayerSpeed = 0f;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
 }
--- Assets/SourceCodes/System/Interfaces.cs
+++
@@ -1,6 +0,0 @@
-// インターフェイス集約ファイル
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-
