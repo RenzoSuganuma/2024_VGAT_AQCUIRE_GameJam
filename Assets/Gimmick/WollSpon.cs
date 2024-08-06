669b088fae175e1c80c9f0494bcd05e06580aca2
RenzoSuganuma <103351677+RenzoSuganuma@users.noreply.github.com>
2024-08-06 14:07:07 +0900

    Revert "Revert "[add] 「壁が動くスプリクトを作った」「壁を生成のスプリクトを作った」""

---
+++ Assets/Gimmick/WollSpon.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WollSpon : MonoBehaviour
+{
+    [SerializeField] GameObject _Woll;
+    [SerializeField] float _Time = 3f;
+    float _Timer = 0;
+     GameSystem _GameSystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _GameSystem = GameObject.FindAnyObjectByType<GameSystem>();
+        if (!_GameSystem.IsPausing)
+        {
+            Instantiate(_Woll, this.transform.position, Quaternion.identity);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!_GameSystem.IsPausing)
+        {
+            _Timer += Time.deltaTime;
+            if (_Timer > _Time)
+            {
+                _Timer = 0;
+                Instantiate(_Woll, this.transform.position, Quaternion.identity);
+            }
+        }
+        
+    }
+}
