bfda2ce69402ca9ebe765193c92c7e66cd91ce73
KakinumaYuzuki <V.proYuzu23@gmail.com>
2024-08-06 23:31:15 +0900

    [ add, update, fix ] アニメーションの追加、状態変更予兆の追加、プレハブの修正

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -10,7 +12,7 @@ public sealed class PlayerController : MonoBehaviour
     [SerializeField, Header("切り替わる時間の最小時間(秒)")] private float _minChangeTime = 3f;
     [SerializeField, Header("切り替わる時間の最大時間(秒)")] private float _maxChangeTime = 10f;
     [SerializeField, Header("跳ぶときに力を加え続ける高さの限界")] private float _y = 0.5f;      
-    
+    [SerializeField, Header("切り替え予兆の時間")]　private float _time = 2f;//ToDO:命名考える
     // ギミックから
     [SerializeField, Header("飛ぶときにアウトな壁のタグ")] private string _flyGOTag = "";
     [SerializeField, Header("跳ぶときにアウトな壁のタグ")] private string _jumpGOTag = "";
@@ -19,6 +21,7 @@ public sealed class PlayerController : MonoBehaviour
     [SerializeField] private AudioClip _flyAudioClip;
     [SerializeField] private AudioClip _jumpAudioClip;
     [SerializeField] private AudioClip _clashAudioClip;
+    [SerializeField] private AudioClip _changeAudioClip; // ToDo:命名考える＆予兆の音を入れる
     
     private Rigidbody _rb;
     private GameSystem _gameSystem;
@@ -28,20 +31,23 @@ public sealed class PlayerController : MonoBehaviour
     private bool _isInputJump = false;
     private bool _isKeyDown = false;
     private bool _isKeyUp = false;
-    
+
+    public PlayerMoveState CurrentMoveState => _currentMoveState;
+
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
-        Debug.Log(_currentMoveState);
+        Debug.Log(CurrentMoveState);
         _gameSystem = GameObject.FindObjectOfType<GameSystem>().GetComponent<GameSystem>();
         _audioSource = GetComponent<AudioSource>();
-        _animator = GetComponent<Animator>();
-        // ランダムに飛ぶと跳ぶを変更
-        //StartCoroutine("ChangePlayerMoveState");
+        _animator = GetComponentInChildren<Animator>();
+        // ToDo:ランダムに飛ぶと跳ぶを変更
+        StartCoroutine("ChangePlayerMoveState");
         if (_flyGOTag is null || _jumpGOTag is null)
         {
             Debug.LogWarning("ゲームオーバー用のタグがnullです");
         }
+        //_animator.Play();
     }
 
     private void FixedUpdate()
@@ -71,7 +77,7 @@ public sealed class PlayerController : MonoBehaviour
 
     private void PlayerMove()
     {
-        switch (_currentMoveState)
+        switch (CurrentMoveState)
         {
             case PlayerMoveState.Fly:
                 FlyPlayerMove();
@@ -149,13 +155,29 @@ public sealed class PlayerController : MonoBehaviour
     }
 
     /// <summary>
-    /// 接地判定とゲームオーバーの接触判定
+    /// 接地判定
     /// </summary>
     /// <param name="other"></param>
     private void OnCollisionEnter(Collision other)
     {
         string tag = other.gameObject.tag;
-        if (_currentMoveState == PlayerMoveState.Fly)
+        if (CurrentMoveState == PlayerMoveState.Jump)
+        {
+            if (tag == "Ground")
+            {
+                _isGrounded = true;
+            }            
+        }
+    }
+
+    /// <summary>
+    /// ゲームオーバー判定
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        string tag = other.gameObject.tag;
+        if (CurrentMoveState == PlayerMoveState.Fly)
         {
             // Flyの時にぶつかったらぶつかったらダメなオブジェクトにぶつかるとゲームオーバー
             if (_flyGOTag is not null && tag == _flyGOTag)
@@ -163,34 +185,57 @@ public sealed class PlayerController : MonoBehaviour
                 GameOver();
             }
         }
-        else if (_currentMoveState == PlayerMoveState.Jump)
+        else if (CurrentMoveState == PlayerMoveState.Jump)
         {
             // Jumpの時にぶつかったらぶつかったらダメなオブジェクトにぶつかるとゲームオーバー
             if (_jumpGOTag is not null && tag == _jumpGOTag)
             {
                 GameOver();
             }
-            if (tag == "Ground")
-            {
-                _isGrounded = true;
-            }            
         }
     }
-    
+
     /// <summary>
     /// ランダムな時間経過で操作方法を切り替える
     /// </summary>
     /// <returns></returns>
     IEnumerator ChangePlayerMoveState()
     {
+        var time = 0f;
         while (true)
         {
+            // ２秒前に予兆(アニメーションと音)
+            //_currentMoveState = PlayerMoveState.Fly; // ToDo:ここいらないかも
+            
+                        
+            _animator.SetBool("ChangeBefore", false);
+            _audioSource.Stop();
             _currentMoveState = PlayerMoveState.Fly;
             Debug.Log("Flyに変更");
-            yield return new WaitForSeconds(Random.Range(_minChangeTime, _maxChangeTime));
-            Debug.Log("Jumpに変更");
+            yield return new WaitForSeconds(Random.Range(_minChangeTime, _maxChangeTime - _time));
+            
+            _animator.SetBool("ChangeBefore", true);
+            _audioSource.PlayOneShot(_changeAudioClip);
+            Debug.Log($"{_time}秒後に状態をJumpに切り替えます");
+            yield return new WaitForSeconds(_time);
+            
+            _animator.SetBool("ChangeBefore", false);
+            _audioSource.Stop();
             _currentMoveState = PlayerMoveState.Jump;
-            yield return new WaitForSeconds(Random.Range(_minChangeTime, _maxChangeTime));
+            Debug.Log("Jumpに変更");
+            yield return new WaitForSeconds(Random.Range(_minChangeTime, _maxChangeTime - _time));
+            
+            _animator.SetBool("ChangeBefore", true);
+            _audioSource.PlayOneShot(_changeAudioClip);
+            Debug.Log($"{_time}秒後に状態をFlyに切り替えます");
+            yield return new WaitForSeconds(_time);
+            
+            /*_animator.SetBool("ChangeBefore", false);
+            _currentMoveState = PlayerMoveState.Fly;
+            Debug.Log("Flyに変更");
+            yield return new WaitForSeconds(Random.Range(_minChangeTime, _maxChangeTime - _time));*/
         }
     }
+    // ToDo:当たったらダメなところはトリガー
+    // 設置はコライダー
 }
