3374e210ad1e989e6bf0ef1897dcbe74d5614927
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2024-08-06 11:14:39 +0900

    [ add ,update ] チュートリアルシーンの追加とシステム系クラスのアプデ

--- Assets/SourceCodes/System/GameSystem.cs
+++ Assets/SourceCodes/System/GameSystem.cs
@@ -1,44 +1,111 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 ///  ゲームシステムクラス
 /// </summary>
-public class GameSystem : MonoBehaviour
+public sealed class GameSystem : MonoBehaviour
 {
+    [SerializeField, Header("加速度 [m/s^2]")]
+    private float _acceleration = 1.0f;
+
+    [SerializeField, Header("速度の最大値 [m/s]")]
+    private float _maxVelocity = 10.0f;
+
     /// <summary>
     /// ポーズ入力が入ったときに呼び出してほしいメソッドはここ
     /// </summary>
     public event Action ToDoOnPause;
+
     /// <summary>
     /// リジューム入力が入ったときに呼び出してほしいメソッドはここ
     /// </summary>
     public event Action ToDoOnResume;
 
+    private bool _isPausing = false;
+
+    public bool IsPausing => _isPausing;
+
+    private bool _isJumping = false;
+
     /// <summary>
     /// ジャンプ入力 【トリガー】
     /// </summary>
-    public bool IsJumping => Input.GetKeyDown(KeyCode.Space);
+    public bool IsJumping => _isJumping;
+
+    private float _velocity = 1.0f;
 
     /// <summary>
-    /// プレイヤのスピードを返す
+    /// 速度ベクトルの大きさ
     /// </summary>
-    public float PlayerSpeed = 0f;
+    public float PlayerVelocityMagnitude => _velocity;
+
+    private float _elapsedTime = 0f;
+
+    private void Awake()
+    {
+        // DDOL 登録
+        GameObject.DontDestroyOnLoad(this.gameObject);
+    }
 
     private void Start()
     {
-        
     }
 
     private void Update()
     {
-        
+        CheckPauseInput();
+        GetPlayerVelocity();
+        _isJumping = Input.GetKeyDown(KeyCode.Space);
+    }
+
+    private void CheckPauseInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab)) // 仮の一時停止キー
+        {
+            _isPausing = !_isPausing;
+            if (_isPausing)
+            {
+                if (ToDoOnPause is not null)
+                {
+                    ToDoOnPause(); // pause
+                }
+            }
+            else
+            {
+                if (ToDoOnResume is not null)
+                {
+                    ToDoOnResume(); // resume
+                }
+            }
+        }
     }
 
     private void FixedUpdate()
     {
-        
+        _elapsedTime += Time.fixedDeltaTime;
+    }
+
+    private void GetPlayerVelocity()
+    {
+        _velocity = _acceleration * _elapsedTime; // 後でここは直す
+
+        if (_maxVelocity < _velocity)
+        {
+            _velocity = _maxVelocity;
+        } // クランプ処理
+    }
+
+    /// <summary>
+    /// DDOl登録の解除とオブジェクトの破棄の実行をする
+    /// </summary>
+    public void DisposeThisObject()
+    {
+        var scene = SceneManager.GetActiveScene();
+        SceneManager.MoveGameObjectToScene(this.gameObject, scene);
     }
 }
