949b57c0ec2cd0ce83585eca16f558af4a0029bb
takasimamasahiro <134473651+takasimamasahiro@users.noreply.github.com>
2024-08-07 12:12:57 +0900

    [add] [ 壁と床をプレイヤーの状態に応じて光らせる　スプリクトとPrefabを作成 ]

---
+++ Assets/Gimmick/SafeAreaLighting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeAreaLighting : MonoBehaviour
+{
+    PlayerController _PlayerState;
+    [SerializeField, Header("壁のライトを入れてください")] GameObject _AreaLight;
+    Light _Light;
+    [SerializeField, Header("床の光数値")] float _GroundLightIntensity = 400f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _Light =   GetComponent<Light>();
+        _PlayerState = GameObject.FindAnyObjectByType<PlayerController>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_PlayerState.CurrentMoveState == PlayerMoveState.Fly)
+        {
+            _Light.intensity = 0;
+        }
+        else if (_PlayerState.CurrentMoveState == PlayerMoveState.Jump)
+        {
+            _Light.intensity = _GroundLightIntensity;
+        }
+    }
+}
---
+++ Assets/Gimmick/WallSafeAreaLight.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSafeAreaLight : MonoBehaviour
+{
+    PlayerController _PlayerState;
+    [SerializeField, Header("壁のライトを入れてください")] GameObject _AreaLight;
+    Light _Light;
+    [SerializeField , Header("壁の光数値")]  float _WallLightIntensity = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _Light = GetComponent<Light>();
+        _PlayerState = GameObject.FindAnyObjectByType<PlayerController>();
+
+        if (_PlayerState.CurrentMoveState == PlayerMoveState.Fly)
+        {
+            _Light.intensity = 0;
+        }
+        else if (_PlayerState.CurrentMoveState == PlayerMoveState.Jump)
+        {
+            _Light.intensity = _WallLightIntensity;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    //    if (_PlayerState.CurrentMoveState == PlayerMoveState.Fly)
+    //    {
+    //        _Light.intensity = 0;
+    //    }
+    //    else if (_PlayerState.CurrentMoveState == PlayerMoveState.Jump)
+    //    {
+    //        _Light.intensity = 400;
+    //    }
+    }
+}
