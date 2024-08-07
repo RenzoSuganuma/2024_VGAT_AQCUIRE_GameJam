660a82d772d273246c7afc9f210e6b5c8fb2987d
RyotaTsushima <ryota.00.08.08@gmail.com>
2024-08-07 11:28:05 +0900

    [ update ] ボタンのImageをラフ版から正式版に変更

--- Assets/SourceCodes/System/ResultSysytem.cs
+++ Assets/SourceCodes/System/ResultSysytem.cs
@@ -10,25 +10,12 @@ public class ResultSysytem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //ScoreText.color = new Color(1, 1, 1, 0);
         ScoreText.text = GameSystem.PlayerScore;
-        //StartCoroutine(TextFade());
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    public void PlaySound()
-    {
-        Instantiate(SoundPrefab);
-    }
-
-    IEnumerator TextFade()
-    {
-        yield return new WaitForSeconds(0.3f);
-        ScoreText.CrossFadeAlpha(1, 0.1f, false);
     }
 }
